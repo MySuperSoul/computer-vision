c6a5de0917ba5bea0f94ba4b993a583f22027ad0
huangyifei <huangyifei@fabu.ai>
2019-11-22 13:06:59 +0800

    refine image processor structure

--- assignments/assignment1/ImageProcessor.cc
+++ assignments/assignment1/ImageProcessor.cc
@@ -10,81 +10,79 @@ int kstore = 20;
 int kfunc_num = 4;
 }  // namespace
 
+void ImageProcessor::SetValues(const cv::Mat &last_frame,
+                               const cv::Mat &current_frame) {
+  last_frame_ = last_frame;
+  current_frame_ = current_frame;
+  rows_ = last_frame_.rows;
+  cols_ = last_frame_.cols;
+  center_x_ = cols_ / 2;
+  center_y_ = rows_ / 2;
+  per_iter_ = cols_ / kstore;
+  per_iter_x_ = per_iter_ / 2;
+  per_iter_y_ = rows_ / (2 * kstore);
+  image_ = cv::Mat::ones(rows_, cols_, CV_8UC3);
+}
+
 void ImageProcessor::SlowlyChangeThroughImages(
-    const cv::Mat &last_frame, const cv::Mat &current_frame,
     std::vector<cv::Mat> *concat_images) {
   double weight;
   for (int i = 1; i <= kstore; i++) {
     weight = i * 1.0 / kstore;
-    cv::Mat image;
-    cv::addWeighted(last_frame, 1 - weight, current_frame, weight, 0.0, image);
-    concat_images->push_back(image);
+    cv::addWeighted(last_frame_, 1 - weight, current_frame_, weight, 0.0,
+                    image_);
+    concat_images->push_back(image_.clone());
   }
 }
 
-void ImageProcessor::ShiftFromLeft(const cv::Mat &last_frame,
-                                   const cv::Mat &current_frame,
-                                   std::vector<cv::Mat> *concat_images) {
-  int cols = last_frame.cols;
-  int per_iter = cols / kstore;
-  cv::Mat image = cv::Mat::ones(last_frame.rows, last_frame.cols, CV_8UC3);
+void ImageProcessor::ShiftFromLeft(std::vector<cv::Mat> *concat_images) {
   for (int i = 0; i < kstore; i++) {
-    int start_pos = i * per_iter + 1;
-    int end_pos = std::min(start_pos + per_iter, cols);
-    current_frame.colRange(start_pos, end_pos)
-        .copyTo(image.colRange(start_pos, end_pos));
-    concat_images->push_back(image.clone());
+    int start_pos = i * per_iter_ + 1;
+    int end_pos = std::min(start_pos + per_iter_, cols_);
+    current_frame_.colRange(start_pos, end_pos)
+        .copyTo(image_.colRange(start_pos, end_pos));
+    concat_images->push_back(image_.clone());
   }
 }
 
-void ImageProcessor::ShiftFromRight(const cv::Mat &last_frame,
-                                    const cv::Mat &current_frame,
-                                    std::vector<cv::Mat> *concat_images) {
-  int cols = last_frame.cols;
-  int per_iter = cols / kstore;
-  cv::Mat image = cv::Mat::ones(last_frame.rows, last_frame.cols, CV_8UC3);
+void ImageProcessor::ShiftFromRight(std::vector<cv::Mat> *concat_images) {
   for (int i = 0; i < kstore; i++) {
-    int end_pos = std::min(cols, cols - i * per_iter + 1);
-    int start_pos = std::max(0, end_pos - per_iter);
-    current_frame.colRange(start_pos, end_pos)
-        .copyTo(image.colRange(start_pos, end_pos));
-    concat_images->push_back(image.clone());
+    int end_pos = std::min(cols_, cols_ - i * per_iter_ + 1);
+    int start_pos = std::max(0, end_pos - per_iter_);
+    current_frame_.colRange(start_pos, end_pos)
+        .copyTo(image_.colRange(start_pos, end_pos));
+    concat_images->push_back(image_.clone());
   }
 }
 
-void ImageProcessor::ExpandFromMiddle(const cv::Mat &last_frame,
-                                      const cv::Mat &current_frame,
-                                      std::vector<cv::Mat> *concat_images) {
-  int cols = last_frame.cols, rows = last_frame.rows;
-  int center_x = cols / 2, center_y = rows / 2;
-  int per_iter_x = cols / (2 * kstore), per_iter_y = rows / (2 * kstore);
-
-  cv::Mat image = cv::Mat::ones(last_frame.rows, last_frame.cols, CV_8UC3);
+void ImageProcessor::ExpandFromMiddle(std::vector<cv::Mat> *concat_images) {
   for (int i = 1; i <= kstore; i++) {
-    int x = center_x - per_iter_x * i, y = center_y - per_iter_y * i;
-    current_frame(cv::Rect(x, y, 2 * i * per_iter_x, 2 * i * per_iter_y))
-        .copyTo(image(cv::Rect(x, y, 2 * i * per_iter_x, 2 * i * per_iter_y)));
-    concat_images->push_back(image.clone());
+    int x = center_x_ - per_iter_x_ * i, y = center_y_ - per_iter_y_ * i;
+    current_frame_(cv::Rect(x, y, 2 * i * per_iter_x_, 2 * i * per_iter_y_))
+        .copyTo(
+            image_(cv::Rect(x, y, 2 * i * per_iter_x_, 2 * i * per_iter_y_)));
+    concat_images->push_back(image_.clone());
   }
 }
 
 void ImageProcessor::GetConcatImages(const cv::Mat &last_frame,
                                      const cv::Mat &current_frame,
                                      std::vector<cv::Mat> *concat_images) {
+  SetValues(last_frame, current_frame);
+
   rand_index_ = cv_common::Util::GetRandNum(1, kfunc_num);
-  std::cout << rand_index_ << std::endl;
   switch (rand_index_) {
     case 1:
-      SlowlyChangeThroughImages(last_frame, current_frame, concat_images);
+      SlowlyChangeThroughImages(concat_images);
       break;
     case 2:
-      ShiftFromLeft(last_frame, current_frame, concat_images);
+      ShiftFromLeft(concat_images);
       break;
     case 3:
-      ShiftFromRight(last_frame, current_frame, concat_images);
+      ShiftFromRight(concat_images);
       break;
     case 4:
-      ExpandFromMiddle(last_frame, current_frame, concat_images);
+      ExpandFromMiddle(concat_images);
       break;
     default:
       return;
--- assignments/assignment1/ImageProcessor.h
+++ assignments/assignment1/ImageProcessor.h
@@ -16,20 +16,26 @@ class ImageProcessor {
   void GetConcatImages(const cv::Mat &last_frame, const cv::Mat &current_frame,
                        std::vector<cv::Mat> *concat_images);
 
-  void SlowlyChangeThroughImages(const cv::Mat &last_frame,
-                                 const cv::Mat &current_frame,
-                                 std::vector<cv::Mat> *concat_images);
-  void ShiftFromRight(const cv::Mat &last_frame, const cv::Mat &current_frame,
-                      std::vector<cv::Mat> *concat_images);
+  void SlowlyChangeThroughImages(std::vector<cv::Mat> *concat_images);
 
-  void ShiftFromLeft(const cv::Mat &last_frame, const cv::Mat &current_frame,
-                     std::vector<cv::Mat> *concat_images);
+  void ShiftFromRight(std::vector<cv::Mat> *concat_images);
 
-  void ExpandFromMiddle(const cv::Mat &last_frame, const cv::Mat &current_frame,
-                        std::vector<cv::Mat> *concat_images);
+  void ShiftFromLeft(std::vector<cv::Mat> *concat_images);
+
+  void ExpandFromMiddle(std::vector<cv::Mat> *concat_images);
+
+  void SetValues(const cv::Mat &last_frame, const cv::Mat &current_frame);
 
  private:
+  cv::Mat last_frame_;
+  cv::Mat current_frame_;
+  cv::Mat image_;
+
   int rand_index_;
+  int cols_, rows_;
+  int per_iter_;
+  int per_iter_x_, per_iter_y_;
+  int center_x_, center_y_;
 };
 
 }  // namespace assignment1
